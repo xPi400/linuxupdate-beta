5b07af356c08739ec6cb3ccdd0220604459ca507
X Pi 400 <139676935+xPi400@users.noreply.github.com>
2023-07-16 03:10:47 -0500

    Update debian-apt.sh

--- debian-apt.sh
+++ debian-apt.sh
@@ -1,7 +1,7 @@
-echo Starting PUARU script...
+echo Starting LinuxUpdate script...
 sudo apt update || error 'Could not APT Update!'
 sudo apt autoremove || error 'Could not APT Autoremove!'
 sudo apt update || error 'Could not APT Update!'
 sudo apt full-upgrade || error 'Could not APT Full Upgrade!'
 sudo apt autoclean || error 'Could not APT Autoclean!'
-echo PUARU script finished! Now exiting... || exit 1
+echo LinuxUpdate script finished! Now exiting... || exit 1
